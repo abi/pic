1d47d3c61351cbad916161bddb806b47f1e80a74
Abi Raja <abimanyuraja@gmail.com>
2013-10-17 13:25:13 -0700

    API: GET endpoints and testing for these endpoints.

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ var SimpleApp = require('./lib/SimpleApp')
 var timeago = require('timeago')
 var _ = require('underscore')
 
+function d (msg) {
+  console.log(msg)
+}
+
 function App (cb, opts_) {
   var self = this
 
@@ -43,11 +47,116 @@ App.prototype.init = function () {
     return marked(str)
   }
 
-  app.get('/new', self.auth, function (req, res) {
-    res.render('new')
+  // API
+  // ========
+
+  var PICS_PER_PAGE = 20
+
+  // Get most recent pics
+  // Params
+  //  * page: page number
+  // Possible errors:
+  //  * Internal error (when could this happen?)
+  app.get('/pics/new', function (req, res) {
+    var page = req.query.page || 1
+
+    models.Pic
+      .find({})
+      .skip((page - 1) * PICS_PER_PAGE)
+      .sort('-created').limit(PICS_PER_PAGE)
+      .exec(function (err, pics) {
+        if (err) {
+          d(error)
+          res.send(500, {code: 'INTERNAL_ERROR'})
+        } else {
+          res.send(200, {pics: pics, page: page})
+        }
+    })
   })
 
-  app.post('/photos/upload', self.auth, function (req, res) {
+  // Get most popular pics
+  // Params
+  //  * page: page number
+  // Possible errors:
+  //  * Internal error (when could this happen?)
+  //  For now, it's the same as /pics/new
+  app.get('/pics/popular', function (req, res) {
+    var page = req.query.page || 1
+
+    models.Pic
+      .find({})
+      .skip((page - 1) * PICS_PER_PAGE)
+      .sort('-created').limit(PICS_PER_PAGE)
+      .exec(function (err, pics) {
+        if (err) {
+          d(error)
+          res.send(500, {code: 'INTERNAL_ERROR'})
+        } else {
+          res.send(200, {pics: pics, page: page})
+        }
+    })
+  })
+
+  // Get most pics posted by a particular user
+  // Params
+  //  * page: page number
+  // Possible errors:
+  //  * Internal error (when could this happen?)
+  //  * User not found
+  app.get('/pics/user/:id', function (req, res) {
+    var page = req.query.page || 1
+    var userId = req.params.id
+
+    models.User.findById(userId, function (err, user) {
+      var idCastError = err &&
+                        (err.name === 'CastError') &&
+                        (err.type === 'ObjectId')
+
+      if (user === null || idCastError) {
+        res.send(500, {code: 'ID_NOT_FOUND', detail: 'User ID not found'})
+      } else if (err) {
+        d(err)
+        res.send(500, {code: 'INTERNAL_ERROR'})
+      } else {
+        models.Pic
+          .find({user: user})
+          .skip((page - 1) * PICS_PER_PAGE)
+          .sort('-created').limit(PICS_PER_PAGE)
+          .exec(function (err, pics) {
+            if (err) {
+              d(err)
+              res.send(500, {code: 'INTERNAL_ERROR'})
+            } else {
+              res.send(200, {pics: pics, page: page})
+            }
+        })
+      }
+    })
+  })
+
+  // Get a pic by its id.
+  // Returns all metadata associated with that pic
+  // Possible errors:
+  //  * ID does not exist
+  //  * Internal error
+  app.get('/pics/:id', function (req, res) {
+    models.Pic.findById(req.params.id, function (err, pic) {
+      var idCastError = err &&
+                        (err.name === 'CastError') &&
+                        (err.type === 'ObjectId')
+
+      if (pic === null || idCastError) {
+        res.send(500, {code: 'ID_NOT_FOUND', detail: 'Pic ID not found'})
+      } else if (err) {
+        d(err)
+        res.send(500, {code: 'INTERNAL_ERROR'})
+      } else {
+        res.send(200, pic.toJSON())
+      }
+    })
+  })
+
+  app.post('/pics/upload', self.auth, function (req, res) {
     var pic = new models.Pic({
       title: req.body.title,
       caption: req.body.caption,
@@ -63,47 +172,6 @@ App.prototype.init = function () {
     })
   })
 
-  // app.post('/edit', self.auth, function (req, res) {
-  //   var id = req.body.id
-  //   models.Post.findById(id, function (err, post) {
-  //     if (post === null || post.user.toString() !== req.user.id) {
-  //       console.error(new Error('Post #' + id + ' could not found.'))
-  //       res.render('error', {error: 'That post could not be found. Did you delete it perhaps?'})
-  //     } else if (err) {
-  //       console.error(err)
-  //       res.render('error', {error: 'Oops, something bad happening. We\' looking into it. Sorry!'})
-  //     } else {
-  //       res.render('edit', {post: post})
-  //     }
-  //   })
-  // })
-  //
-
-  // API to get a single image information
-  app.get('/photos/:id', function (req, res) {
-
-  })
-
-  // Blog page
-  app.get('/photos/:id', function (req, res) {
-    var username = req.params.username
-    models.User.findOne({username: username}, function (err, blogger) {
-      if (blogger === null) {
-        res.render('404')
-      } else if (err) {
-        res.send(500, {error: err})
-      } else {
-        models.Post
-          .find({user: blogger})
-          .sort('-created').limit(10)
-          .exec(function (err, posts) {
-          res.render('blog', {posts: posts,
-                              blogger: blogger,
-                              isBlogger: req.user ? (req.user.id === blogger.id) : false})
-        })
-      }
-    })
-  })
 }
 
 exports.App = App
--- test/api.js
+++ test/api.js
@@ -2,12 +2,18 @@ var App = require('../index').App
 var assert = require('assert')
 var request = require('request')
 
+function d (msg) {
+  console.log(msg)
+}
+
 describe('Pic', function(){
 
   var PORT = 9998 // TODO: Use a random port
   var BASE = 'http://localhost:' + PORT + '/'
+
   var USER = 'abi'
   var PASS = 'password'
+  var USER_ID = '525f435a58e202dc28000001'
 
   before(function (done) {
 
@@ -32,11 +38,11 @@ describe('Pic', function(){
   describe('upload', function () {
     it('should add the image to the database', function (done) {
       request.post({
-        url: BASE + 'photos/upload',
+        url: BASE + 'pics/upload',
         form: {title: 'test', caption: 'testy'},
         jar: true
       }, function (error, response, body) {
-        assert(response.statusCode === 200)
+        assert.equal(response.statusCode, 200)
         picId = JSON.parse(body)._id
         done()
       })
@@ -44,7 +50,138 @@ describe('Pic', function(){
     })
   })
 
+  describe('get pic by id', function () {
+    it('should return the previously inserted image when queried by ID', function (done) {
+      request.get({
+        url: BASE + 'pics/' + picId,
+      }, function (error, response, body) {
+        assert.equal(response.statusCode, 200)
+        var pic = JSON.parse(body)
+        assert.equal(pic.title, 'test')
+        done()
+      })
+    })
+
+    // The difference between wrong and nonsense ID is a subtle implementation
+    // detail. It does not conern the end-user of the API.
+
+    it('should return an error when queried by wrong ID', function (done) {
+      request.get({
+        url: BASE + 'pics/' + '4' + picId.slice(1),
+      }, function (error, response, body) {
+        assert.equal(response.statusCode, 500)
+        var error = JSON.parse(body)
+        assert.equal(error.code, 'ID_NOT_FOUND')
+        done()
+      })
+    })
+
+    it('should return an error when queried by nonsense ID', function (done) {
+      request.get({
+        url: BASE + 'pics/' + picId.slice(1),
+      }, function (error, response, body) {
+        assert.equal(response.statusCode, 500)
+        var error = JSON.parse(body)
+        assert.equal(error.code, 'ID_NOT_FOUND')
+        done()
+      })
+    })
+  })
+
+  describe('get new pics', function () {
+    it('should return the 20 most recent pics', function (done) {
+      request.get({
+        url: BASE + 'pics/new',
+      }, function (error, response, body) {
+        assert(response.statusCode === 200)
+        var res = JSON.parse(body)
+        assert.equal(res.pics.length, 20)
+        assert.equal(res.pics[0].title, 'test')
+        assert.equal(res.page, 1)
+        done()
+      })
+    })
+
+    it('when asked for page 10, it should return no pics', function (done) {
+      request.get({
+        url: BASE + 'pics/new?page=10',
+      }, function (error, response, body) {
+        assert(response.statusCode === 200)
+        var res = JSON.parse(body)
+        assert.equal(res.pics.length, 0)
+        assert.equal(res.page, 10)
+        done()
+      })
+    })
+  })
+
+  describe('get popular pics', function () {
+    it('should return the 20 most recent pics', function (done) {
+      request.get({
+        url: BASE + 'pics/popular',
+      }, function (error, response, body) {
+        assert(response.statusCode === 200)
+        var res = JSON.parse(body)
+        assert.equal(res.pics.length, 20)
+        assert.equal(res.pics[0].title, 'test')
+        assert.equal(res.page, 1)
+        done()
+      })
+    })
+
+    it('when asked for page 10, it should return no pics', function (done) {
+      request.get({
+        url: BASE + 'pics/popular?page=10',
+      }, function (error, response, body) {
+        assert.equal(response.statusCode, 200)
+        var res = JSON.parse(body)
+        assert.equal(res.pics.length, 0)
+        assert.equal(res.page, 10)
+        done()
+      })
+    })
+  })
+
+  describe('get pics by a user', function () {
+    it('should return the 20 most recent pics', function (done) {
+      request.get({
+        url: BASE + 'pics/user/' + USER_ID,
+      }, function (error, response, body) {
+        assert.equal(response.statusCode, 200)
+        var res = JSON.parse(body)
+        assert.equal(res.pics.length, 20)
+        assert.equal(res.pics[0].title, 'test')
+        assert.equal(res.page, 1)
+        done()
+      })
+    })
+
+    it('when asked for page 10, it should return no pics', function (done) {
+      request.get({
+        url: BASE + 'pics/user/' + USER_ID + '?page=10',
+      }, function (error, response, body) {
+        assert.equal(response.statusCode, 200)
+        var res = JSON.parse(body)
+        assert.equal(res.pics.length, 0)
+        assert.equal(res.page, 10)
+        done()
+      })
+    })
+
+    it('when asked for pics by a non-existent user, it should return an error', function (done) {
+      request.get({
+        url: BASE + 'pics/user/' + USER_ID.slice(1) + '?page=10',
+      }, function (error, response, body) {
+        assert.equal(response.statusCode, 500)
+        var res = JSON.parse(body)
+        assert.equal(res.code, 'ID_NOT_FOUND')
+        done()
+      })
+    })
+
+  })
+
   after(function () {
-    console.log('Inserted pic with id ' + picId)
+    // TODO: Clean up the test database
   })
 })
